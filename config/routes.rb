0433b37263a356f75604cc7638bb1118bab61caa
Assad Isah <nottherealalanturing@gmail.com>
2022-10-29 23:53:05 +0100

    add user and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      #resources :messages, only: [:index]
+      post "/signup", to: "users#create"
+      post "/login", to: "sessions#create"
+      get "/authorized", to: "sessions#show"
+    end
+  end
 end
