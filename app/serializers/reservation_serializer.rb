781d251682efe9343c9a7f55fa5b810e3522af69
Assad Isah <nottherealalanturing@gmail.com>
2022-11-02 16:53:30 +0100

    resolve linter errors with rubocop

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -2,14 +2,10 @@ class ReservationSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :car_id, :reservation_date, :location, :model, :car, :user
 
   def car
-    {
-      car_details: object.car
-    }
+    object.car
   end
 
   def user
-    {
-      user_details: object.user
-    }
+    object.user
   end
 end
