ef74f55c3c27e576ffad94ab4422413511989b59
myelin0 <emi.veisllari@gmail.com>
2022-11-08 19:56:25 +0100

    Add show car spec working

--- spec/requests/api/v1/cars_spec.rb
+++ spec/requests/api/v1/cars_spec.rb
@@ -29,69 +29,60 @@ RSpec.describe 'api/v1/cars', type: :request do
     end
   end
 
-  # path '/api/v1/add_car' do
+  path '/api/v1/add_car' do
 
-  #   post('add_car car') do
+    post('add_car car') do
       
-  #     tags 'Cars'
-  #     consumes 'application/json'
-  #     produces 'application/json'
-  #     parameter name: :car, in: :body, schema: {
-  #       type: :object,
-  #       properties: {
-  #         name: { type: :string },
-  #         cost: { type: :integer },
-  #         description: {type: :string},
-  #         owner:{type: :integer},
-  #         image_url: {type: :string}
-  #       },
-  #       required: [ 'name', 'cost','description', 'image_url', 'owner']
-  #     }
-  #     response(200, 'successful') do
-  #       # let(:Authorization){"#{token}"}
-  #         let(:car){{name:'Honda',cost: 300, description:'Nice car', owner: 6, image:'https://www.istockphoto.com/photo/pink-retro-toy-car-isolated-on-white-gm1386831709-444935852?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcars&utm_term=cars%3A%3A%3A'}}
-  #       # after do |example|
-  #       #   example.metadata[:response][:content] = {
-  #       #     'application/json' => {
-  #       #       example: JSON.parse(response.body, symbolize_names: true)
-  #       #     }
-  #       #   }
-  #       # end
-  #       run_test!
-  #     end
-  #   end
-  # end
+      tags 'Cars'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :car, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          cost: { type: :integer },
+          description: {type: :string},
+          owner:{type: :integer},
+          image_url: {type: :string}
+        },
+        required: [ 'name', 'cost','description', 'image_url', 'owner']
+      }
+      response(200, 'successful') do
+        # let(:Authorization){"#{token}"}
+          let(:car){{name:'Honda',cost: 300, description:'Nice car', owner: 6, image:'https://www.istockphoto.com/photo/pink-retro-toy-car-isolated-on-white-gm1386831709-444935852?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcars&utm_term=cars%3A%3A%3A'}}
+        # after do |example|
+        #   example.metadata[:response][:content] = {
+        #     'application/json' => {
+        #       example: JSON.parse(response.body, symbolize_names: true)
+        #     }
+        #   }
+        # end
+        run_test!
+      end
+    end
+  end
 
-  # path '/api/v1/show_car' do
+  path '/api/v1/show_car/{id}' do
 
-  #   get('show_car car') do
-  #     tags 'Cars'
-  #     produces 'application/json'
-  #     security [bearerAuth: []]
-  #     parameter name: :id, in: :path, type: :string
-  #     response(200, 'successful') do
-  #       let(:Authorization){"Bearer #{token}"}
-  #       schema type: :object,
-  #       properties:{
-  #         name: {type: :string},
-  #         image: {type: :string},
-  #         cost: {type: :integer},
-  #         description: {type: :string}
-  #       }
-  #       let(:Authorization){"Bearer #{issue_token(User.last)}"}
-  #       let (:id) do
-  #         Car.create(name:'Mustang', image:'',cost:3000,description:"Fast and endurable")
-  #       # after do |example|
-  #       #   example.metadata[:response][:content] = {
-  #       #     'application/json' => {
-  #       #       example: JSON.parse(response.body, symbolize_names: true)
-  #       #     }
-  #       #   }
-  #       # end
-  #       run_test!
-  #     end
-  #   end
-  # end
+    get('show_car car') do
+      tags 'Cars'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :string
+      response(200, 'successful') do
+        schema type: :object,
+        properties:{
+          name: {type: :string},
+          image_url: {type: :string},
+          cost: {type: :integer},
+          description: {type: :string}
+        }
+        let (:id) do
+          Car.create(name:'Mustang', image_url:'',cost:3000,description:"Fast and endurable")
+        end
+        run_test!
+      end
+    end
+  end
 
   # path '/api/v1/cars/{id}' do
   #   # You'll want to customize the parameter types...
